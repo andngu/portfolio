55509534c80099f8e7c982cf56e39340c118a3b5
Andrew <andrewnguyen0204@gmail.com>
2023-06-06 16:13:29 -0500

    fixed linting rules

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -47,9 +47,9 @@ function NavBar() {
         ))}
       </ul>
 
-      <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 textgray-500 md:hidden">
+      <button type="button" onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 textgray-500 md:hidden">
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-      </div>
+      </button>
 
       {nav && (
       <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
