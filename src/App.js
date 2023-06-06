f67bae8aafcec4eba30e873a830cf3b75ba1b3b3
Andrew <andrewnguyen0204@gmail.com>
2023-06-06 16:05:32 -0500

    added linting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import Socials from "./components/Socials";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact"
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Socials from './components/Socials';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
-    return (
-        <div>
-            <NavBar />
-            <Home />
-            <Projects />
-            <Skills />
-            <Contact />
-            <Socials />
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Socials />
+    </div>
+  );
 }
 
 export default App;
