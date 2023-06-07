e4dec8da5c76cd7578163cae482053b9c143b408
Andrew <andrewnguyen0204@gmail.com>
2023-06-07 08:43:05 -0500

    lint/prettier formatting

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,61 +1,58 @@
-import React from 'react'
-import test from '../assets/test.png'
+import test from "../assets/test.png";
 
-const Projects = () => {
-    const projects = [
-        {
-            id: 1,
-            src: test,
-        },
-        {
-            id: 2,
-            src: test,
-        },
-        {
-            id: 3,
-            src: test,
-        },
-        {
-            id: 4,
-            src: test,
-        },
-        {
-            id: 5,
-            src: test,
-        },
-        {
-            id: 6,
-            src: test,
-        },
-    ];
+function Projects() {
+   const projects = [
+      {
+         id: 1,
+         src: test,
+      },
+      {
+         id: 2,
+         src: test,
+      },
+      {
+         id: 3,
+         src: test,
+      },
+      {
+         id: 4,
+         src: test,
+      },
+      {
+         id: 5,
+         src: test,
+      },
+      {
+         id: 6,
+         src: test,
+      },
+   ];
 
-    return (
-        <div name="projects" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
-            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-                <div className="pb-8 flex flex-row justify-center">
-                    <p className="text-5xl font-bold inline border-gray-500">Projects</p>
-                </div>
-
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
-                    {projects.map(({ id, src }) => (
-                        <div key={id} className="shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-110">
-                            <img src={src}
-                                alt=""
-                                className="rounded-md " />
-                            <div className="flex items-center justify-center">
-                                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 border-b">
-                                    Demo
-                                </button>
-                                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 border-b">
-                                    Code
-                                </button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+   return (
+      <div name="projects" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8 flex flex-row justify-center">
+               <p className="text-5xl font-bold inline border-gray-500">Projects</p>
             </div>
-        </div>
-    )
+
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
+               {projects.map(({ id, src }) => (
+                  <div key={id} className="shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-110">
+                     <img src={src} alt="" className="rounded-md " />
+                     <div className="flex items-center justify-center">
+                        <button type="button" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 border-b">
+                           Demo
+                        </button>
+                        <button type="button" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 border-b">
+                           Code
+                        </button>
+                     </div>
+                  </div>
+               ))}
+            </div>
+         </div>
+      </div>
+   );
 }
 
-export default Projects
+export default Projects;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,84 +1,81 @@
-import React from 'react'
-import nodeSVG from '../assets/nodejs-icon.svg'
-import cssSVG from '../assets/css-3.svg'
-import graphqlSVG from '../assets/graphql.svg'
-import htmlSVG from '../assets/html-5.svg'
-import javascriptSVG from '../assets/javascript.svg'
-import mongodbSVG from '../assets/mongodb-icon.svg'
-import reactSVG from '../assets/react.svg'
-import tailwindSVG from '../assets/tailwindcss-icon.svg'
+import nodeSVG from "../assets/nodejs-icon.svg";
+import cssSVG from "../assets/css-3.svg";
+import graphqlSVG from "../assets/graphql.svg";
+import htmlSVG from "../assets/html-5.svg";
+import javascriptSVG from "../assets/javascript.svg";
+import mongodbSVG from "../assets/mongodb-icon.svg";
+import reactSVG from "../assets/react.svg";
+import tailwindSVG from "../assets/tailwindcss-icon.svg";
 
-const Skills = () => {
-    const technologies = [
-        {
-            id: 1,
-            src: javascriptSVG,
-            title: 'Javascript',
-            style: 'shadow-yellow-500'
-        },
-        {
-            id: 2,
-            src: htmlSVG,
-            title: 'HTML',
-            style: 'shadow-orange-500'
-        },
-        {
-            id: 3,
-            src: cssSVG,
-            title: 'CSS',
-            style: 'shadow-blue-500'
-        },
-        {
-            id: 4,
-            src: tailwindSVG,
-            title: 'Tailwind CSS',
-            style: 'shadow-cyan-500'
-        },
-        {
-            id: 5,
-            src: reactSVG,
-            title: 'React',
-            style: 'shadow-[#61dbfb]'
-        },
-        {
-            id: 6,
-            src: graphqlSVG,
-            title: 'GraphQL',
-            style: 'shadow-pink-400'
-        },
-        {
-            id: 7,
-            src: mongodbSVG,
-            title: 'MongoDB',
-            style: 'shadow-green-500'
-        },
-        {
-            id: 8,
-            src: nodeSVG,
-            title: 'NodeJS',
-            style: 'shadow-[#3c873a]'
-        },
-    ];
-    return (
-        <div name="skills" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
-            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-                <div className="pb-8 flex flex-row justify-center">
-                    <p className="text-5xl font-bold inline border-gray-500">Skills</p>
-                </div>
-
-                <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-                    {technologies.map(({ id, src, title, style }) => (
-                        <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                            <img src={src} alt="" className="w-20 mx-auto" />
-                            <p className="mt-4">{title}</p>
-                        </div>
-                    ))}
-
-                </div>
-
+function Skills() {
+   const technologies = [
+      {
+         id: 1,
+         src: javascriptSVG,
+         title: "Javascript",
+         style: "shadow-yellow-500",
+      },
+      {
+         id: 2,
+         src: htmlSVG,
+         title: "HTML",
+         style: "shadow-orange-500",
+      },
+      {
+         id: 3,
+         src: cssSVG,
+         title: "CSS",
+         style: "shadow-blue-500",
+      },
+      {
+         id: 4,
+         src: tailwindSVG,
+         title: "Tailwind CSS",
+         style: "shadow-cyan-500",
+      },
+      {
+         id: 5,
+         src: reactSVG,
+         title: "React",
+         style: "shadow-[#61dbfb]",
+      },
+      {
+         id: 6,
+         src: graphqlSVG,
+         title: "GraphQL",
+         style: "shadow-pink-400",
+      },
+      {
+         id: 7,
+         src: mongodbSVG,
+         title: "MongoDB",
+         style: "shadow-green-500",
+      },
+      {
+         id: 8,
+         src: nodeSVG,
+         title: "NodeJS",
+         style: "shadow-[#3c873a]",
+      },
+   ];
+   return (
+      <div name="skills" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8 flex flex-row justify-center">
+               <p className="text-5xl font-bold inline border-gray-500">Skills</p>
             </div>
-        </div>
-    )
+
+            <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+               {technologies.map(({ id, src, title, style }) => (
+                  <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                     <img src={src} alt="" className="w-20 mx-auto" />
+                     <p className="mt-4">{title}</p>
+                  </div>
+               ))}
+            </div>
+         </div>
+      </div>
+   );
 }
 
-export default Skills
+export default Skills;
